96560b2acab39b1b8e4ed69294ccd3c396f36c66
saikrishs <sai.srungavarapu@gmail.com>
2024-11-05 08:12:15 -0600

    downgraded version

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom'; // Ensure this is imported from 'react-dom', not 'react-dom/client'
+import './index.css';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
+// Correct usage of ReactDOM.render
+ReactDOM.render(
     <StrictMode>
         <App />
-    </StrictMode>
+    </StrictMode>,
+    document.getElementById('root') // This should be the second argument
 );
